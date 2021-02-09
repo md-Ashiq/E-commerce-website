cab9cfa384bae2ccedb9101202c42728fdcb9860
Ashiq <kashiq1999@gmail.com>
2021-02-09 16:05:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const config={
+    type:"carousel",
+    perView:1,
+    dots: '#dots',
+}
+new Glide('.glide',config).mount();
+
+/* var btn= document.querySelector(".btn-more");
+var boxes= document.querySelectorAll(".off-boxes");
+/* var box_1= document.querySelector(".off-box-1");
+var box_2= document.querySelector(".off-box-2");
+var box_3= document.querySelector(".off-box-3"); */
+/* console.log(hide);
+boxes.forEach.document.addEventListener("mouseover", hide);
+ function hide(){
+     btn.style.display="initial";
+     return;
+ } */ 
